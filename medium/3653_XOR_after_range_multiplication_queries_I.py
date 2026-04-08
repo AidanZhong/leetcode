5706e5b6bfd3ad9c21bf10ecc37aaf6e34e86ab5
50601 <506017129@qq.com>
2026-04-08 17:25:49 +0100

    3653 refactor loop and simplify modulo calculation

--- medium/3653_XOR_after_range_multiplication_queries_I.py
+++ medium/3653_XOR_after_range_multiplication_queries_I.py
@@ -35,14 +35,13 @@ from typing import List
 
 class Solution:
     def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        MOD = 10 ** 9 + 7
         for l, r, k, v in queries:
-            idx = l
-            while idx <= r:
-                nums[idx] = (nums[idx] * v) % (10 ** 9 + 7)
-                idx += k
+            for idx in range(l, r + 1, k):
+                nums[idx] = nums[idx] * v % MOD
 
         ans = 0
-        for i in nums:
-            ans ^= i
+        for x in nums:
+            ans ^= x
         return ans
 
