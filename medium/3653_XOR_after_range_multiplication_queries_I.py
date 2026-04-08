c90adaee80989361ace5f26e710699b2d8970c1b
50601 <506017129@qq.com>
2026-04-08 13:48:57 +0100

    3653

---
+++ medium/3653_XOR_after_range_multiplication_queries_I.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 08/04/2026 13:45
+
+@author: Aidan
+@project: leetcode
+@filename: 3653_XOR_after_range_multiplication_queries_I
+
+You are given an integer array nums of length n and a 2D integer array queries of size q, where queries[i] = [li, ri, ki, vi].
+
+For each query, you must apply the following operations in order:
+
+Set idx = li.
+While idx <= ri:
+Update: nums[idx] = (nums[idx] * vi) % (109 + 7)
+Set idx += ki.
+Return the bitwise XOR of all elements in nums after processing all queries.
+
+
+
+Example 1:
+
+Input: nums = [1,1,1], queries = [[0,2,1,4]]
+
+Output: 4
+
+Explanation:
+
+A single query [0, 2, 1, 4] multiplies every element from index 0 through index 2 by 4.
+The array changes from [1, 1, 1] to [4, 4, 4].
+The XOR of all elements is 4 ^ 4 ^ 4 = 4.
+"""
+from typing import List
+
+
+class Solution:
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        for l, r, k, v in queries:
+            idx = l
+            while idx <= r:
+                nums[idx] = (nums[idx] * v) % (10 ** 9 + 7)
+                idx += k
+
+        ans = 0
+        for i in nums:
+            ans ^= i
+        return ans
+
